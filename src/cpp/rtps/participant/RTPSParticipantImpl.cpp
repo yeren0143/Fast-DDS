3c0b2d3d42145afb0c9385556caa40a088139ff5
Ricardo González Moreno <ricardo@richiware.dev>
2021-08-30 08:31:04 +0200

    Refs #12355. Separate initialization and enable of BuiltinProtocols
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/cpp/rtps/participant/RTPSParticipantImpl.cpp
+++ src/cpp/rtps/participant/RTPSParticipantImpl.cpp
@@ -410,6 +410,14 @@ RTPSParticipantImpl::RTPSParticipantImpl(
         logInfo(RTPS_PARTICIPANT,
                 "RTPSParticipant \"" << m_att.getName() << "\" with guidPrefix: " << m_guid.guidPrefix);
     }
+
+    // Start builtin protocols
+    if (!mp_builtinProtocols->initBuiltinProtocols(this, m_att.builtin))
+    {
+        logError(RTPS_PARTICIPANT, "The builtin protocols were not correctly initialized");
+    }
+
+    logInfo(RTPS_PARTICIPANT, "RTPSParticipant \"" << m_att.getName() << "\" with guidPrefix: " << m_guid.guidPrefix);
 }
 
 RTPSParticipantImpl::RTPSParticipantImpl(
@@ -425,11 +433,7 @@ RTPSParticipantImpl::RTPSParticipantImpl(
 
 void RTPSParticipantImpl::enable()
 {
-    // Start builtin protocols
-    if (!mp_builtinProtocols->initBuiltinProtocols(this, m_att.builtin))
-    {
-        logError(RTPS_PARTICIPANT, "The builtin protocols were not correctly initialized");
-    }
+    mp_builtinProtocols->enable();
 
     //Start reception
     for (auto& receiver : m_receiverResourcelist)
